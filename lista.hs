b8c3195fed2b412254912aa9bcfac8debab4985a
Henrique Fugie de Macedo <113394526+henriquefugie@users.noreply.github.com>
2023-05-30 11:38:30 -0300

    Update lista.hs

--- lista.hs
+++ lista.hs
@@ -7,7 +7,12 @@ dist_pontos (x1, y1) (x2, y2) = sqrt((x2 - x1)^2 + (y2 - y1)^2)
 {-2) Faça uma função para verificar se um ano informado é bissexto ou não.-}
 
 ano_bissexto :: Int -> Bool
-ano_bissexto x = x `mod` 4 == 0
+ano_bissexto x = (x `mod` 4 == 0 && x `mod` 100 /= 0 || x `mod` 400 == 0)
+
+ano_bissextog :: Int -> Bool
+ano_bissextog x | (x `mod` 4 == 0 && x `mod` 100 /= 0) = True
+                | (x `mod` 400 == 0) = True
+                | otherwise = False
 
 {-3) Defina uma função que recebe três números inteiros representando,
 respectivamente, um dia, um mês e um ano e verifica se os números formam uma
@@ -24,4 +29,3 @@ conceito correspondentes conforme as regras abaixo:
 Nota abaixo de 4 – Conceito E, Nota entre 4 e 5.99 conceito D, Nota
 entre 6 e 7.49 conceito C, Intervalo entre 7.5 e 8.99 conceito B e acima de 9
 conceito A.-}
-
