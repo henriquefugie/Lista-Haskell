d5f221c075af78ba875a363d246e06fbe40e695f
Henrique Fugie de Macedo <113394526+henriquefugie@users.noreply.github.com>
2023-05-30 11:11:16 -0300

    Update lista.hs

---
+++ lista.hs
@@ -0,0 +1,26 @@
+{-1) Faça uma função que calcule a distância entre dois pontos-}
+--d=√((x_2-x_1)²+(y_2-y_1)²) 
+
+dist_pontos :: (Float, Float) -> (Float, Float) -> Float
+dist_pontos (x1, y1) (x2, y2) = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+
+{-2) Faça uma função para verificar se um ano informado é bissexto ou não.-}
+
+
+
+{-3) Defina uma função que recebe três números inteiros representando,
+respectivamente, um dia, um mês e um ano e verifica se os números formam uma
+data válida.-}
+
+
+
+{-4) Crie uma função par::Int->Bool para verificar se um numero é par ou impar.-}
+
+
+
+{-5) Escreva a função conceito :: Float -> Char que recebe uma nota e retorne o
+conceito correspondentes conforme as regras abaixo:
+Nota abaixo de 4 – Conceito E, Nota entre 4 e 5.99 conceito D, Nota
+entre 6 e 7.49 conceito C, Intervalo entre 7.5 e 8.99 conceito B e acima de 9
+conceito A.-}
+
